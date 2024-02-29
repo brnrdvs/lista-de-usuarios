a06326d0d06f83c1b9f12795955ab64ca7d81faa
brnrdvs <bernardostraube@hotmail.com>
2024-02-29 10:49:41 -0300

    alteracoes

--- teste/src/components/item/item.jsx
+++ teste/src/components/item/item.jsx
@@ -1,8 +1,17 @@
+import {useNavigate} from "react-router-dom";
+
+
 function Item ( { item } ) {
+    const navegar = useNavigate();
+
+    const clicar = () => {
+        navegar(`/detalhes/${item.id}`);
+    }
+
     return (
         <li>
-            <p>NOME: { item.name }</p>
-            <p>ID: { item.id }</p>
+            <p>NOME: { item.nome }</p>
+            <button type="button" onClick={clicar}>VER DETALHES</button>
         </li>
     )
 }
